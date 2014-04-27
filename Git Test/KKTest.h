e037455641a7dadc5431ee867028abac06de36c0
kentran08 <kentran@Kens-MacBook-Pro.local>
2014-04-27 01:57:34 -0700

    added a test class

---
+++ Git Test/KKTest.h
@@ -0,0 +1,13 @@
+//
+//  KKTest.h
+//  Git Test
+//
+//  Created by Ken Tran on 2014-04-27.
+//  Copyright (c) 2014 Ken Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKTest : NSObject
+
+@end
---
+++ Git Test/KKTest.m
@@ -0,0 +1,13 @@
+//
+//  KKTest.m
+//  Git Test
+//
+//  Created by Ken Tran on 2014-04-27.
+//  Copyright (c) 2014 Ken Tran. All rights reserved.
+//
+
+#import "KKTest.h"
+
+@implementation KKTest
+
+@end
